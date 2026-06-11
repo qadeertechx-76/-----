9133648707e481be48cba883e584417e6e3572ee
qadeertechx-76 <qadeertechx@gmail.com>
2026-06-11 11:09:23 +0500

    Add files via upload

---
+++ ecosystem.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  apps : [{
+    name: "Qadeer-KD-mini",
+    script: "./server.js",
+    watch: false,
+    autorestart: true,
+    max_memory_restart: '2G',
+    env: {
+      NODE_ENV: "production",
+    }
+  }]
+};
---
+++ settings.js
@@ -0,0 +1,6 @@
+module.exports = {
+    giphyApiKey: process.env.GIPHY_API_KEY || 'dc6zaTOxFJmzC',
+    ownerNumber: process.env.OWNER_NUMBER || '923000000000',
+    botName: 'Qadeer-KD-mini',
+    ownerName: 'Qadeer-TechX'
+};
